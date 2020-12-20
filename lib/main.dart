be3a17278aff6efef0ffe18b1cbf3717b13001ab
MyMoi <titolivier@gmx.fr>
2020-12-20 18:46:51 +0100

    add configuration file (api url)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 //import 'package:qr_flutter/qr_flutter.dart';
 import 'package:qr_web_client/pages/talk_page.dart';
 
@@ -16,15 +19,26 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+// decode our json
+//final json = jsonDecode(contents);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Qr transfer'),
-      ),
-      body: Center(
+        appBar: AppBar(
+          title: Text('Qr transfer'),
+        ),
+        body: Center(
           child: //TalkPage(),)
-              ConnectedPage(host: "192.168.1.27:8000")),
-    );
+              FutureBuilder(
+            future: rootBundle.loadString('assets/config.json'),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return ConnectedPage(host: jsonDecode(snapshot.data)["apiUrl"]);
+              } else {
+                return CircularProgressIndicator();
+              }
+            },
+          ),
+        ));
   }
 }
